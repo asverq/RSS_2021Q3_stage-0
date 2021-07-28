fa87e643d6e29b648db0d11dd27ba355740dcef2
Igor Arinichev <igor.arin@gmail.com>
2021-07-28 17:32:09 +0300

    feat: implement video section

--- museum/app/js/main.js
+++ museum/app/js/main.js
@@ -1,6 +1,15 @@
-$(function(){
+$(function () {});
 
+const progress = document.querySelector(".progress-bar");
 
+progress.addEventListener("input", function () {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #fff ${value}%, white 100%)`;
+});
 
-  
+const progress2 = document.querySelector(".volume-bar");
+
+progress2.addEventListener("input", function () {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #fff ${value}%, white 100%)`;
 });

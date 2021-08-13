51d12a9248bc7088f7d1aef96fd5fbec687b7ed0
Igor Arinichev <igor.arin@gmail.com>
2021-08-13 21:11:56 +0300

    feat: implement drag and drop sliding alfa version

--- vertical-slider/app.js
+++ vertical-slider/app.js
@@ -1,106 +1,163 @@
-const upBtn = document.querySelector('.up-button'),
-    downBtn = document.querySelector('.down-button'),
-    sidebar = document.querySelector('.sidebar'),
-    sliderContainer = document.querySelector('.container'),
-    mainSlide = document.querySelector('.main-slide'),
-    allSlides = document.getElementsByClassName('slide'),
-    allSideSlides = document.getElementsByClassName('side-slide'),
-    height = mainSlide.clientHeight,
-    duration = 900
+function verticalSlider() {
+    const upBtn = document.querySelector('.up-button'),
+        downBtn = document.querySelector('.down-button'),
+        sidebar = document.querySelector('.sidebar'),
+        sliderContainer = document.querySelector('.container'),
+        mainSlide = document.querySelector('.main-slide'),
+        allSlides = document.getElementsByClassName('slide'),
+        allSideSlides = document.getElementsByClassName('side-slide'),
+        height = mainSlide.clientHeight,
+        duration = 700
 
-let scrollable = true
+    let scrollable = true
 
-function setCloneSlides() {
-    // Clone MainSlide Slides
-    const firstSlide = allSlides[0],
-        cloneFirst = firstSlide.cloneNode(true),
-        lastSlide = allSlides[allSlides.length - 1],
-        cloneLast = lastSlide.cloneNode(true)
+    function setCloneSlides() {
+        // Clone MainSlide Slides
+        const firstSlide = allSlides[0],
+            cloneFirst = firstSlide.cloneNode(true),
+            lastSlide = allSlides[allSlides.length - 1],
+            cloneLast = lastSlide.cloneNode(true)
 
-    mainSlide.appendChild(cloneFirst)
-    mainSlide.insertBefore(cloneLast, firstSlide)
+        mainSlide.appendChild(cloneFirst);
+        mainSlide.insertBefore(cloneLast, firstSlide);
 
-    //Clone Sidebar Slides
-    const firstSideSlide = allSideSlides[0],
-        cloneSideFirst = firstSideSlide.cloneNode(true),
-        lastSideSlide = allSideSlides[allSideSlides.length - 1],
-        cloneSideLast = lastSideSlide.cloneNode(true)
+        //Clone Sidebar Slides
+        const firstSideSlide = allSideSlides[0],
+            cloneSideFirst = firstSideSlide.cloneNode(true),
+            lastSideSlide = allSideSlides[allSideSlides.length - 1],
+            cloneSideLast = lastSideSlide.cloneNode(true)
 
-    sidebar.appendChild(cloneSideFirst)
-    sidebar.insertBefore(cloneSideLast, firstSideSlide)
-}
-setCloneSlides()
+        sidebar.appendChild(cloneSideFirst);
+        sidebar.insertBefore(cloneSideLast, firstSideSlide);
+    }
+    setCloneSlides()
 
-const countSlides = allSlides.length
-let activeSlideIndex = 1
+    const countSlides = allSlides.length
+    let activeSlideIndex = 1
 
-function setPosition() {
-    mainSlide.style.transitionDuration = "0s"
-    sidebar.style.transitionDuration = "0s"
-    setActiveSlide()
-    setTimeout(normalaizeDuration, 0);
-    sidebar.style.top = `-${(countSlides - 1) * 100}vh`
-}
-setPosition()
-
-document.addEventListener('keydown', (event) => {
-    event.preventDefault()
-    if (event.key === 'ArrowUp') scrollable === false || changeSlide('up')
-    if (event.key === 'ArrowDown') scrollable === false || changeSlide('down')
-})
-
-sliderContainer.addEventListener('mousewheel', event => {
-    event.preventDefault()
-    if (event.deltaY === -100) scrollable === false || changeSlide('up')
-    if (event.deltaY === 100) scrollable === false || changeSlide('down')
-})
-
-upBtn.addEventListener('click', () => scrollable === false || changeSlide('up'))
-downBtn.addEventListener('click', () => scrollable === false || changeSlide('down'))
-
-mainSlide.addEventListener('transitionend', (event) => {
-    if (event.returnValue === true) scrollable = true
-})
-
-
-function changeSlide(direction) {
-    if (direction === 'up' && activeSlideIndex !== countSlides - 2 && activeSlideIndex !== countSlides - 1) {
-        scrollable = false
-        activeSlideIndex++
+    function setPosition() {
+        blockDuration()
         setActiveSlide()
-    } else if (direction === 'up' && activeSlideIndex === countSlides - 2) {
-        scrollable = false
-        activeSlideIndex++
-        setActiveSlide()
-        setTimeout(() => resetActiveIndex(1), duration + 100)
+        setTimeout(normalaizeDuration, 0);
+        sidebar.style.top = `-${(countSlides - 1) * 100}vh`
+    }
+    setPosition()
+
+    document.addEventListener('keydown', (event) => {
+        event.preventDefault()
+        if (event.key === 'ArrowUp') scrollable === false || changeSlide('up')
+        if (event.key === 'ArrowDown') scrollable === false || changeSlide('down')
+    })
+
+    sliderContainer.addEventListener('mousewheel', event => {
+        event.preventDefault()
+        if (event.deltaY === -100) scrollable === false || changeSlide('up')
+        if (event.deltaY === 100) scrollable === false || changeSlide('down')
+    })
+
+    upBtn.addEventListener('click', () => scrollable === false || changeSlide('up'))
+    downBtn.addEventListener('click', () => scrollable === false || changeSlide('down'))
+
+    sliderContainer.addEventListener('dragstart', scrollable === false || dragStart)
+    sliderContainer.addEventListener('drag', scrollable === false || dragAction)
+    sliderContainer.addEventListener('dragend', scrollable === false || dragEnd)
+
+    let startPosY
+    let endPosY
+    let inDrag
+
+    function dragStart(event) {
+        sliderContainer.style.cursor = 'grab'
+        console.log(event);
+        startPosY = event.clientY
     }
 
-    if (direction === 'down' && activeSlideIndex !== 1 && activeSlideIndex !== 0) {
-        scrollable = false
-        activeSlideIndex--
+    function dragAction(event) {
+        event.preventDefault()
+        sliderContainer.style.cursor = 'grabbing'
+        blockDuration()
+        inDrag = startPosY - event.clientY
+        mainSlide.style.transform = `translateY(-${height * activeSlideIndex + inDrag}px)`
+        sidebar.style.transform = `translateY(${height * activeSlideIndex + inDrag}px)`
+    }
+
+    function dragEnd(event) {
+        sliderContainer.style.cursor = 'default'
+        endPosY = event.clientY
+        normalaizeDuration()
+       
+        detectDrag()
+    }
+
+    function detectDrag() {
+        if (scrollable === true) {
+            if (Math.abs(startPosY - endPosY) > 15) {
+                if (startPosY - endPosY > 0) {
+                    mainSlide.style.transform = `translateY(-${height * activeSlideIndex - inDrag}px)`
+                    sidebar.style.transform = `translateY(${height * activeSlideIndex - inDrag}px)`
+                    changeSlide('up')
+                } else {
+                    mainSlide.style.transform = `translateY(-${height * activeSlideIndex - inDrag}px)`
+                    sidebar.style.transform = `translateY(${height * activeSlideIndex - inDrag}px)`
+                    changeSlide('down')
+                }
+            }
+        }
+    }
+
+    function unblockScrollable() {
+        scrollable = true
+    }
+
+    function changeSlide(direction) {
+        if (direction === 'up' && activeSlideIndex !== countSlides - 2 && activeSlideIndex !== countSlides - 1) {
+            scrollable = false
+            activeSlideIndex++
+            setActiveSlide()
+            setTimeout(unblockScrollable, duration + 50)
+        } else if (direction === 'up' && activeSlideIndex === countSlides - 2) {
+            scrollable = false
+            activeSlideIndex++
+            setActiveSlide()
+            setTimeout(() => resetActiveIndex(1), duration + 100)
+            setTimeout(unblockScrollable, duration + 50)
+        }
+
+        if (direction === 'down' && activeSlideIndex !== 1 && activeSlideIndex !== 0) {
+            scrollable = false
+            activeSlideIndex--
+            setActiveSlide()
+            setTimeout(unblockScrollable, duration + 50)
+        } else if (direction === 'down' && activeSlideIndex === 1) {
+            scrollable = false
+            activeSlideIndex--
+            setActiveSlide()
+            setTimeout(() => resetActiveIndex(countSlides - 2), duration + 100)
+            setTimeout(unblockScrollable, duration + 50)
+        }
+    }
+
+    function setActiveSlide() {
+        mainSlide.style.transform = `translateY(-${height * activeSlideIndex}px)`
+        sidebar.style.transform = `translateY(${height * activeSlideIndex}px)`
+    }
+
+    function resetActiveIndex(index) {
+        blockDuration()
+        activeSlideIndex = index
         setActiveSlide()
-    } else if (direction === 'down' && activeSlideIndex === 1) {
-        scrollable = false
-        activeSlideIndex--
-        setActiveSlide()
-        setTimeout(() => resetActiveIndex(countSlides - 2), duration + 100)
+        setTimeout(normalaizeDuration, 0)
+    }
+
+    function blockDuration() {
+        mainSlide.style.transitionDuration = "0s"
+        sidebar.style.transitionDuration = "0s"
+    }
+
+    function normalaizeDuration() {
+        mainSlide.style.transitionDuration = `${duration / 1000}s`
+        sidebar.style.transitionDuration = `${duration / 1000}s`
     }
 }
 
-function setActiveSlide() {
-    mainSlide.style.transform = `translateY(-${height * activeSlideIndex}px)`
-    sidebar.style.transform = `translateY(${height * activeSlideIndex}px)`
-}
-
-function resetActiveIndex(index) {
-    activeSlideIndex = index
-    mainSlide.style.transitionDuration = "0s"
-    sidebar.style.transitionDuration = "0s"
-    setActiveSlide()
-    setTimeout(normalaizeDuration, 0)
-}
-
-function normalaizeDuration() {
-    mainSlide.style.transitionDuration = `${duration / 1000}s`
-    sidebar.style.transitionDuration = `${duration / 1000}s`
-}
+verticalSlider()

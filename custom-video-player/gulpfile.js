e0907e3852813ba15416ad246c2b6a731153470f
Igor Arinichev <igor.arin@gmail.com>
2021-08-22 23:38:18 +0300

    feat: add video directory to gulp config

--- custom-video-player/gulpfile.js
+++ custom-video-player/gulpfile.js
@@ -88,6 +88,8 @@ gulp.task('export', async function () {
 
   let BuildImg = gulp.src('app/img/**/*.*')
     .pipe(gulp.dest('dist/img'));
+  let BuildVideo = gulp.src('app/video/**/*.*')
+    .pipe(gulp.dest('dist/video'));
 });
 
 gulp.task('watch', function () {
